34c86856f0a6a5c35e2d475b1f56e71ac42f8685
mike-t <miseven95@gmail.com>
2019-06-02 22:19:20 +1000

    Initial commit

---
+++ app/src/main/java/com/mititch/inventoryctrl/InventoryCtrlApplication.java
@@ -0,0 +1,13 @@
+package com.mititch.inventoryctrl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryCtrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryCtrlApplication.class, args);
+	}
+
+}
